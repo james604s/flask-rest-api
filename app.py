8072925e00c2602a1ee4625cba458b5d223eb5e0
Squid Tseng <47516926+james604s@users.noreply.github.com>
2021-06-19 20:04:34 +0800

    Feature, JWT Auththentication_And_Login

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+from security import authenticate, identity
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "jose"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) #/auth
 
 items = []
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # item = list(filter(lambda x: x['name'] ==name, items))
         item = next(filter(lambda x: x['name'] ==name, items), None)
@@ -16,7 +24,7 @@ class Item(Resource):
             return {"message":f"An item with name '{name}' already exists."}, 400
         data = request.get_json()
         item = {"name":name, "price":data['price']}
-        item.append(item)
+        # item.append(item)
         return item, 201
 
 class ItemList(Resource):
